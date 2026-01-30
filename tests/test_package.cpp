5dda38f4df3f4d9db8e17b8ab7a4733a179bccfc
mrw420 <mrw2005.mwj@gmail.com>
2026-01-30 17:51:43 +0100

    Testy 1/3

---
+++ tests/test_package.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include "package.hxx"
+#include "storage_types.hxx"
+
+TEST(PackageTest, IsIdUnique) {
+    Package p1;
+    Package p2;
+    EXPECT_NE(p1.get_id(), p2.get_id());
+}
+
+TEST(PackageQueueTest, FifoOrder) {
+    PackageQueue queue(PackageQueueType::FIFO);
+    queue.push(Package(1));
+    queue.push(Package(2));
+
+    auto p1 = queue.pop();
+    EXPECT_EQ(p1->get_id(), 1); // Pierwszy wszedł, pierwszy wychodzi
+
+    auto p2 = queue.pop();
+    EXPECT_EQ(p2->get_id(), 2);
+}
+
+TEST(PackageQueueTest, LifoOrder) {
+    PackageQueue queue(PackageQueueType::LIFO);
+    queue.push(Package(1));
+    queue.push(Package(2));
+
+    auto p1 = queue.pop();
+    EXPECT_EQ(p1->get_id(), 2); // Ostatni wszedł, pierwszy wychodzi (stos)
+
+    auto p2 = queue.pop();
+    EXPECT_EQ(p2->get_id(), 1);
+}
